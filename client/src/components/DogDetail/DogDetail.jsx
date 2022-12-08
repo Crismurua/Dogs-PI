776262887ce187800d915935e7b5ff57fe3dc13f
Cristian Murua <cristianmurua1995@gmail.com>
2022-12-08 08:54:54 -0300

    fix

--- client/src/components/DogDetail/DogDetail.jsx
+++ client/src/components/DogDetail/DogDetail.jsx
@@ -20,7 +20,7 @@ const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
-  color: theme.palette.text.secondary,
+  color: '#888',
   marginTop: 100
 }));
 
