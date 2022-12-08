895fe69e5e9fbcf34184d3506aff5d1917ef6005
Cristian Murua <cristianmurua1995@gmail.com>
2022-12-08 08:47:47 -0300

    dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,49 @@ import Nav from "./components/Nav/Nav";
 import Container from "@material-ui/core/Container";
 import Landing from "./components/Landing/Landing";
 import EditDog from "./components/EditDog/EditDog";
+import { ThemeProvider, createTheme, CssBaseline, IconButton } from "@material-ui/core";
+import { RiMoonLine, RiSunLine } from 'react-icons/ri';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  darkmode: {
+          position:"absolute",
+          margin:15,
+         
+  },
+}));
+
 
 
 function App() {
+
+  const [dark, setDark] = React.useState(false);
+  const classes = useStyles();
+
+
+  const theme = createTheme({
+    palette: {
+      type: dark ? 'dark' : 'light',
+    }
+  })
+
   return (
-    <Container>
-      <Route exact path='/' component={() => <Landing />}/>
-      <Route path='/dogs' component={() => <Nav />}/>
-      <Switch>
-        <Route exact path='/dogs' component={() => <DogCards />}/>
-        <Route exact path='/dogs/:id' component={() => <DogDetail />}/>
-        <Route exact path='/create' component={() => <CreateDog />}/>
-        <Route exact path='/edit/:id' component={() => <EditDog />}/>
-      </Switch>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <IconButton className={classes.darkmode} edge='start' color="inherit" onClick={()=> setDark(!dark)}>
+        {!dark ? <RiMoonLine /> : <RiSunLine />}
+      </IconButton>
+        <Container>
+          <Route exact path='/' component={() => <Landing />}/>
+          <Route path='/dogs' component={() => <Nav />}/>      
+          <Switch>
+            <Route exact path='/dogs' component={() => <DogCards />}/>
+            <Route exact path='/dogs/:id' component={() => <DogDetail />}/>
+            <Route exact path='/create' component={() => <CreateDog />}/>
+            <Route exact path='/edit/:id' component={() => <EditDog />}/>
+          </Switch>
+        </Container>
+    </ThemeProvider>
   );
 }
 
